8fe4224828dde9d0bf0fffbe220e6c228ddb60eb
rangareddy <rangareddyamireddy48@gmail.com>
2025-02-23 14:56:07 +0530

    3rd file

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -5,10 +5,10 @@ No2=$2
 if [$No1 -gt $No2]
 then 
 echo "$No1 greater than $No2"
-else if [$No1 -lt $No2]
+elif [$No1 -lt $No2]
 then
 echo "$No1 less than $No2"
-else if [$No1 -eq $No2]
+elif [$No1 -eq $No2]
 then
 echo "$No1 equals to $No2"
 else 
